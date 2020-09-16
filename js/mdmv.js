c9d7440e35910635dd8933bd3cbbc400d82aa831
AkshayaAcharya <acharya.akshaya04@gmail.com>
2020-09-16 17:19:57 +0530

    updated

--- js/mdmv.js
+++ js/mdmv.js
@@ -5,12 +5,12 @@ const developers = [
             {
                 id: 1,
                 expName: 'Two Degree of Freedom System',
-                src: '../assets/kushagra/simulation/2dofForced/index.html'
+                src: './assets/kushagra/simulation/2dofForced/index.html'
             },
             {
                 id: 2,
                 expName: 'Vibration Absorber',
-                src: '../assets/kushagra/simulation/VibrationAbsorber/index.html'
+                src: './assets/kushagra/simulation/VibrationAbsorber/index.html'
             }
 
         ]
@@ -21,52 +21,52 @@ const developers = [
             {
                 id: 1,
                 expName: 'Two Degree of Freedom System',
-                src: '../assets/Vaishak/simulation/2dofForced/index.html'
+                src: './assets/Vaishak/simulation/2dofForced/index.html'
             },
             {
                 id: 2,
                 expName: 'Base Excitation',
-                src: '../assets/Vaishak/simulation/BaseExcitation/index.html'
+                src: './assets/Vaishak/simulation/BaseExcitation/index.html'
             },
             {
                 id: 3,
                 expName: 'Cantilever Beam - Free Vibration',
-                src: '../assets/Vaishak/simulation/CantileverBeam/index.html'
+                src: './assets/Vaishak/simulation/CantileverBeam/index.html'
             },
             {
                 id: 4,
                 expName: 'Cantilever Beam - Forced Vibration',
-                src: '../assets/Vaishak/simulation/CantileverBeamForced/index.html'
+                src: './assets/Vaishak/simulation/CantileverBeamForced/index.html'
             },
             {
                 id: 5,
                 expName: 'Fixed Beam - Forced Vibration',
-                src: '../assets/Vaishak/simulation/FixedBeam/index.html'
+                src: './assets/Vaishak/simulation/FixedBeam/index.html'
             },
             {
                 id: 6,
                 expName: 'Forced Vibration',
-                src: '../assets/Vaishak/simulation/ForcedVibration/index.html'
+                src: './assets/Vaishak/simulation/ForcedVibration/index.html'
             },
             {
                 id: 7,
                 expName: 'Simply Supported Beam - Free Vibration',
-                src: '../assets/Vaishak/simulation/SimplySupported/index.html'
+                src: './assets/Vaishak/simulation/SimplySupported/index.html'
             },
             {
                 id: 8,
                 expName: 'Simply Supported Beam - Forced Vibration',
-                src: '../assets/Vaishak/simulation/SimplySupportedForced/index.html'
+                src: './assets/Vaishak/simulation/SimplySupportedForced/index.html'
             },
             {
                 id: 9,
                 expName: 'Rotating Unbalance',
-                src: '../assets/Vaishak/simulation/UnbalancedRotatingMass/index.html'
+                src: './assets/Vaishak/simulation/UnbalancedRotatingMass/index.html'
             },
             {
                 id: 10,
                 expName: 'Vibration Absorber',
-                src: '../assets/Vaishak/simulation/VibrationAbsorber/index.html'
+                src: './assets/Vaishak/simulation/VibrationAbsorber/index.html'
             },
 
         ]
@@ -78,12 +78,12 @@ const developers = [
             {
                 id: 1,
                 expName: 'Base Excitation',
-                src: '../assets/Abhinav/simulation/BaseExcitation/index.html'
+                src: './assets/Abhinav/simulation/BaseExcitation/index.html'
             },
             {
                 id: 2,
                 expName: 'Rotating Unbalance',
-                src: '../assets/Abhinav/simulation/RotatingUnbalance/index.html'
+                src: './assets/Abhinav/simulation/RotatingUnbalance/index.html'
             }
 
         ]
